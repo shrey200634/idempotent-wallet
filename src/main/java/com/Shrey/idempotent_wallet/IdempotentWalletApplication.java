279905aa75c2c51eaa0d6629c7b294326c947464
shrey200634 <dshrey60@gmail.com>
2026-09-02 14:47:26 +0530

    setup the spring project structure with proper configuration with application.properties and all secreats using env

---
+++ src/main/java/com/Shrey/idempotent_wallet/IdempotentWalletApplication.java
@@ -0,0 +1,13 @@
+package com.Shrey.idempotent_wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdempotentWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdempotentWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Shrey/idempotent_wallet/IdempotentWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Shrey.idempotent_wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdempotentWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
