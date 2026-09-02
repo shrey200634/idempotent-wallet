3528be9543806e02edd73bce3147260d9566ced4
shrey200634 <dshrey60@gmail.com>
2026-09-02 15:51:16 +0530

    add the controller for the transation service

---
+++ src/main/java/com/Shrey/idempotent_wallet/controller/TransactionController.java
@@ -0,0 +1,25 @@
+package com.Shrey.idempotent_wallet.controller;
+
+import com.Shrey.idempotent_wallet.dto.TransactionRequest;
+import com.Shrey.idempotent_wallet.dto.TransactionResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/transactions")
+@RequiredArgsConstructor
+public class TransactionController {
+
+    private final com.shrey.wallet.service.TransactionService service;
+
+
+    @PostMapping("/process")
+    public ResponseEntity<TransactionResponse> process(@Valid @RequestBody TransactionRequest request) {
+        return ResponseEntity.ok(service.process(request));
+    }
+}
